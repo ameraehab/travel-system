9bbfbb49068946cfe1534863e284ca6ad20a24bf
Amera Ehab <103929057+ameraehab@users.noreply.github.com>
2024-11-03 16:00:40 +0200

    Create index.js

---
+++ constants/index.js
@@ -0,0 +1,10 @@
+//NAVIGATION
+
+export const NAV_LINKS = [
+    { href: "/", key: "home", lable: "Home" },
+    { href: "/", key: "how_hilink_work", lable: "How Hilink Work" },
+    { href: "/", key: "services", lable: "Services" },
+    { href: "/", key: "pricing", lable: "Pricing" },
+    { href: "/", key: "contact_us", lable: "Contact Us" },
+];
+
