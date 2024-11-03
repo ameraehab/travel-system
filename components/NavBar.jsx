5929a5787ddaef51d753cb3b40a3164c31d56790
Amera Ehab <103929057+ameraehab@users.noreply.github.com>
2024-11-03 16:00:53 +0200

    Update NavBar.jsx

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import { NAV_LINKS } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "./Button";
 
 export const NavBar = () => {
   return (
-    <div>NavBar</div>
+  <nav className="flex justify-between items-center max-w-screen-xl mx-auto px-4  py-5 relative z-30">
+      <Link href="/">
+<Image src="/hilink-logo.svg" alt="Logo" width={74} height={29} />
+
+</Link>
+<ul className=" hidden h-full gap-12 lg:flex">
+{NAV_LINKS.map((link)=>(
+  <Link href={link.href} key={link.key} className="text-base text-gray-600 flex items-center justify-center cursor-pointer pb-1.5 transition-all hover:font-bold ">
+{    link.lable} 
+ </Link>
+))
+}
+
+ </ul>
+ <div className=" lg:flex justify-between items-center hidden">
+<Button 
+type="button"
+title="Login"
+icon="/user.svg"
+variant="btn_dark_green"
+
+/>
+ </div>
+</nav>
   )
 }
 
